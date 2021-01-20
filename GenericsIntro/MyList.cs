0ad3235b457b9edba00784aad1c5ec32795f2eeb
Onur Yiğit <onuryigit54@yaani.com>
2021-01-20 23:51:49 +0300

    20 Ocak

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        //constructor->yapıcı metot (class ile aynı isimli olur)
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)    //item->eleman
+        {
+            T[] tempArray = items;      //elemanların referans numarasını tutmak için geçici dizi oluşturduk
+            items = new T[items.Length+1];      //dizinin eleman sayısını bir artırdık
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] {"Onur","Burcu","Defne","Bilge" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler = new string[5];    //{" "," "," "," "," "}
+            isimler[4] = "Kartal";      //{" "," "," "," ","Kartal"}
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+
+            Console.WriteLine("-------------------------");
+
+
+            List<string> isimler2 = new List<string> {"Onur","Burcu","Defne","Bilge" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            isimler2.Add("Kartal");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
